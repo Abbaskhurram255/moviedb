f109a177895cbcae7501baef2746d062e9a34716
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-06 03:12:35 +0500

    Updates

---
+++ precache-manifest.1530dc29fce6f9d623cd619b8ad4cfe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10f49b91a696ef20dd3ba91a17b479a",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "77884a291f0ced99fe14",
+    "url": "/moviedb/static/css/main.5d12e841.chunk.css"
+  },
+  {
+    "revision": "99d0ffb60db12e2458df",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.9deade40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77884a291f0ced99fe14",
+    "url": "/moviedb/static/js/main.c647fc30.chunk.js"
+  },
+  {
+    "revision": "dff7d7152d1d72c996c0",
+    "url": "/moviedb/static/js/runtime-main.c7335139.js"
+  }
+]);
